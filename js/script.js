d46e634a2d6d414cc243de75b173bc304c59e5ea
Renata Pausini <rpatricia@outlook.com>
2022-06-03 18:57:20 -0300

    header atualizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
-let searchForm = document.querySelector('.search-form')
+let searchForm = document.querySelector('.search-form');
+document.querySelector("#search-btn").onclick = () => {
+
+  searchForm.classList.toggle("active");
+}
+
+let loginForm = document.querySelector(".login-form-container");
+
+document.querySelector("login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+}
+
+document.querySelector("close-login-btn").onclick = () => {
+
+  loginForm.classList.remove("active");
+}
+
 
 window.onscroll = () => {
   searchForm.classList.remove("active");  
+  if(window.screenY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+    
+  }else {
+    document.querySelector(".header .header-2").classList.remove("active")
+  }
 }
+
+window.onload = () => {
+  searchForm.classList.remove("active");  
+  if(window.screenY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+    
+  }else {
+    document.querySelector(".header .header-2").classList.remove("active")
+  }
+}
+ 
